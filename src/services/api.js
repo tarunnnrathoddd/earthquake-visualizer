c8b529f034e25f456821475f1aeab767de4053a7
Tarun Rathod <tarunrathod00805@Taruns-Air.lan>
2024-11-08 16:21:31 +0530

    push all

--- src/services/api.js
+++ src/services/api.js
@@ -3,4 +3,5 @@ import axios from 'axios';
 export const fetchEarthquakeData = async () => {
   const response = await axios.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson');
   return response.data.features;
+
 };
